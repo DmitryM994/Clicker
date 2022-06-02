4b0ebb932ccb5ea9e531f01d4207de104edfd24e
dmitry <malyutin994@gmail.com>
2022-06-02 13:31:22 +0500

    3.5 Базовый функционал кликера

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
 from . import views
 
+boosts = views.BoostViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+})
+
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
     path('login/', views.Login.as_view(), name='login'),
     path('logout/', views.user_logout, name='logout'),
+    #path('call_click/', views.call_click, name='call_click'),
+    path('call_click/', views.call_click, name='call_click'),
+    path('', views.index, name='index'),
+    path('boosts/', boosts, name='boosts'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .forms import UserForm
+from .models import Core, Boost
+from .serializers import CoreSerializer, BoostSerializer
 
 
 class Register(APIView):
@@ -16,6 +20,9 @@ class Register(APIView):
         if form.is_valid():
             user = form.save()
             login(request, user)
+
+            core = Core(user=user)  # Создаем экземпляр класса Core и пихаем в него модель юзера
+            core.save()  # Сохраняем изменения в базу
             return redirect('index')
 
         return render(request, 'register.html', {'form': form})
@@ -35,7 +42,41 @@ class Login(APIView):
         return render(request, 'login.html', {'form': self.form, 'invalid': True})
 
 
+class BoostViewSet(viewsets.ModelViewSet):
+    queryset = Boost.objects.all()
+    serializer_class = BoostSerializer
+
+    def get_queryset(self):
+        core = Core.objects.get(user=self.request.user)
+        boosts = Boost.objects.filter(core=core)
+        return boosts
+
+
+@login_required
+def index(request):
+    core = Core.objects.get(user=request.user)
+    boosts = Boost.objects.filter(core=core)
+
+    return render(request, 'index.html', {
+        'core': core,
+        'boosts': boosts,
+    })
+
+
+@api_view(['GET'])
+@login_required
+def call_click(request):
+    core = Core.objects.get(user=request.user)
+    is_levelup = core.click()
+    if is_levelup:
+        Boost.objects.create(core=core, price=core.level*50, power=core.level*20)
+    core.save()
+
+    return Response({ 'core': CoreSerializer(core).data, 'is_levelup': is_levelup })
+
+
 @login_required
 def user_logout(request):
     logout(request)
     return redirect('login')
+
